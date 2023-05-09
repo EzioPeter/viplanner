86eba59c6ae6d7b2d06406c428cbc400497f7826
Pascal Roth <roth.pascal@outlook.de>
2023-05-09 20:27:15 +0200

    rosbag modifier save compressed images

--- viplanner/rosbag_modifier.py
+++ viplanner/rosbag_modifier.py
@@ -67,20 +67,33 @@ def main(args: argparse.Namespace, m2f_cfg: Mask2FormerCfg):
         sem_img = m2f_wrapper.run_image(img=img)
         sem_img = cv2.cvtColor(sem_img, cv2.COLOR_RGB2BGR)
         # sem_img = cv2.imread("/home/pascal/SemNav/env/anymal/2023_03_23_rsl/semantics/frame_" + str(img_counter).zfill(5) + ".png")
-        
+          
         # Convert the modified image back to an image message
-        modified_msg          = Image()
-        modified_msg.header   = msg.header
-        modified_msg.encoding = "bgr8"
-        modified_msg.height   = sem_img.shape[0]
-        modified_msg.width    = sem_img.shape[1]
-        modified_msg.step     = sem_img.shape[1] * 3
-        modified_msg.data     = np.array(sem_img).tobytes()
+        if args.compressed:
+            # Convert the image to JPEG format
+            success, compressed_image = cv2.imencode('.jpg', sem_img)
+            if not success:
+                raise RuntimeError("Failed to encode image.")
+        
+            modified_msg = CompressedImage()
+            modified_msg.header = msg.header
+            modified_msg.format = "jpeg"
+            modified_msg.data = np.array(compressed_image).tostring()
+        else:
+            modified_msg          = Image()
+            modified_msg.header   = msg.header
+            modified_msg.encoding = "bgr8"
+            modified_msg.height   = sem_img.shape[0]
+            modified_msg.width    = sem_img.shape[1]
+            modified_msg.step     = sem_img.shape[1] * 3
+            modified_msg.data     = np.array(sem_img).tobytes()
         
         # Write the modified image message to the output rosbag file under a new topic
         if topic.endswith("/compressed"):
-            topic = topic[:-len("/compressed")]
-        output_bag.write(topic + "_sem", modified_msg, t)
+            topic = topic[:-len("/compressed")] + "_sem" + "/compressed"
+        else:
+            topic = topic + "_sem"
+        output_bag.write(topic, modified_msg, t)
 
         if args.debug:
             # show prediction
@@ -122,14 +135,17 @@ if __name__ == "__main__":
     # parse args
     parser = argparse.ArgumentParser(description="Predict Semantic Labels for RGB Images in Rosbag")
     parser.add_argument("-bf", "--bag_file", help="Input ROS bag.",
-                        default='/home/pascal/SemNav/env/anymal/2023_03_23_rsl/_2023-03-23-20-30-17.bag')  # '/home/pascal/SemNav/env/anymal/2023_03_27_eth_sun/mergedBag.bag')
-    parser.add_argument("-c", "--topic_rgb", nargs="+", help="Image topics.",
+                        default='/home/pascal/SemNav/env/anymal/2023_01_26_eth/mission_0_all.bag')  # '/home/pascal/SemNav/env/anymal/2023_03_27_eth_sun/mergedBag.bag')
+    parser.add_argument("-t", "--topic_rgb", nargs="+", help="Image topics.",
                         default=[
                             # "/alphasense_driver_ros/cam3/color/image/compressed",  # left
                             # "/alphasense_driver_ros/cam4/color/image/compressed",  # center
                             # "/alphasense_driver_ros/cam5/color/image/compressed",  # right
-                            "/wide_angle_camera_front/image_raw/compressed"
+                            # "/wide_angle_camera_front/image_raw/compressed",
+                            "/wide_angle_camera_front/image_color_rect/compressed",
                         ])
+    parser.add_argument("-c", "--compressed", action="store_false", help="Store the image in the new bag as compressed image",
+                        default=True)    
     parser.add_argument("-d", "--debug", action="store_false", help="Visualize semantic prediction and debug information (assumes img of size 1080x1440)",
                         default=True)
     

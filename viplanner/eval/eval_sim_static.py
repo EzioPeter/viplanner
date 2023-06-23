corrected fear loss computation, 5 point goal loss also for eval functions

viplanner/eval/eval_sim_static.py
@@ -11,15 +11,32 @@ from typing import List, Optional
 from viplanner.config import TrainCfg
 from viplanner.utils.trainer import Trainer
 from viplanner.utils.eval_utils import BaseEvaluator
-from viplanner.traj_cost_opt import TrajCost, TrajOpt
+from viplanner.traj_cost_opt import TrajCost
 
 class SimEvaluator(BaseEvaluator):
     
     debug: bool = False
     
-    def __init__(self, tolerance: float = 0.5, environment: str = "2n8kARJN3HM") -> None:
+    def __init__(
+        self, 
+        tolerance: float = 0.5, 
+        environment: str = "2n8kARJN3HM", 
+        carla: bool = False, 
+        fear_filter: bool = False,
+    ) -> None:
+        """Evaluation of Simulation Results with one-shot predictions
+
+        Args:
+            tolerance (float, optional): Tolerance to classify a goal as reached. Defaults to 0.5.
+            environment (str, optional): Name of the environment used for testing. Defaults to "2n8kARJN3HM".
+            carla (bool, optional): If it is a Carla Simulator environment (affects the DataCfg). Defaults to False.
+        """
+        
         # init base class
-        super().__init__(tolerance)        
+        super().__init__(tolerance)
+        # parameters
+        self.carla: bool = carla
+        self.fear_filter: bool = fear_filter  
         # set random seed for reproducibility
         torch.manual_seed(12)
         # environment
@@ -53,7 +70,32 @@ class SimEvaluator(BaseEvaluator):
         if not use_prev_results or not success:
             # load config
             train_config: TrainCfg = TrainCfg.from_yaml(os.path.join(model_dirs[0], "model.yaml"))
+            # set environment
             train_config.env_list = [self.environment]
+            # set data config
+            if self.carla:
+                if isinstance(train_config.data_cfg, list):
+                    carla_idx = [data_cfg.carla for data_cfg in train_config.data_cfg].index(True)
+                    if carla_idx:
+                        print(f"[INFO] Carla data found, only using first data config: {train_config.data_cfg[carla_idx]}")
+                        train_config.data_cfg = [train_config.data_cfg[carla_idx[0]]]
+                    else:
+                        print(f"[WARNING] No Carla data found, using first data config: {train_config.data_cfg[0]}")
+                        train_config.data_cfg = [train_config.data_cfg[0]]
+                else:
+                    if train_config.data_cfg.carla:
+                        print(f"[INFO] Carla data found: {train_config.data_cfg}")
+                        train_config.data_cfg = [train_config.data_cfg]
+                    else:
+                        print(f"[WARNING] No Carla data found, using data config: {train_config.data_cfg}")
+                        train_config.data_cfg = [train_config.data_cfg]
+            else:
+                if isinstance(train_config.data_cfg, list):
+                    print(f"[INFO] Using first data config: {train_config.data_cfg[0]}")
+                    train_config.data_cfg = [train_config.data_cfg[0]]
+                else:
+                    train_config.data_cfg = [train_config.data_cfg]
+            # enforce that this environment is used for testing
             train_config.test_env_id = 0
             
             # load trainer and data
@@ -140,11 +182,12 @@ class SimEvaluator(BaseEvaluator):
                 goal[inputs[4], 1] = goal[inputs[4], 1] * -1
                 
                 # filter paths with high fear
-                fear_selection = (fear < 0.5).squeeze()
-                preds = preds[fear_selection]
-                goal  = goal[fear_selection]
-                odom  = odom[fear_selection]
-                fear = fear[fear_selection]
+                if self.fear_filter:
+                    fear_selection = (fear < 0.5).squeeze()
+                    preds = preds[fear_selection]
+                    goal  = goal[fear_selection]
+                    odom  = odom[fear_selection]
+                    fear = fear[fear_selection]
                 
                 if len(preds.shape) == 4:
                     # squeeze
@@ -210,23 +253,29 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='Model Eval', description='Evaluate VIPmodels')
     parser.add_argument('-m', '--model_dirs', nargs='+', type=str, help='Path to model directory',
                         default=[
+                            "/home/pascal/SemNav/imperative_learning/models/plannernet_env2azQ1b91cZZ_ep100_inputDepSem_costSem_optimSGD_new_loss_neg05",
                             "/home/pascal/SemNav/imperative_learning/models/plannernet_env2azQ1b91cZZ_ep100_inputDepSem_costSem_optimSGD_neg05",
                             # "/home/pascal/SemNav/imperative_learning/models/plannernet_env2azQ1b91cZZ_ep100_inputDepSem_costSem_optimSGD_combi_more_data_neg05",
-                            "/home/pascal/SemNav/imperative_learning/models/plannernet_env2azQ1b91cZZ_ep100_inputDep_costSem_optimSGD_depth",
+                            # "/home/pascal/SemNav/imperative_learning/models/plannernet_env2azQ1b91cZZ_ep100_inputDep_costSem_optimSGD_depth",
                         ])
     parser.add_argument('-n', '--model_names', nargs='+', type=str, help='Model name',
                     default=[
-                        "VIPlanner",
-                        "iPlanner",
+                        "VIPlanner (new)",
+                        "VIPlanner (old)",
+                        # "iPlanner",
                     ])
     parser.add_argument('-env', '--environment', type=str, help='Environment name',
                 default="2n8kARJN3HM")  # "town01_more_data_train")  # 
+    parser.add_argument('-c', '--carla', action='store_true', help='Use carla environment (changes DataCfg)',
+            default=False)
+    parser.add_argument('-ff', '--fear_filter', action='store_true', help='Filter all fear trajectories for evaluation (filtered fear values above 0.5)',
+            default=False) 
     parser.add_argument('--tolerance', type=float, help='Tolerance to the goal to be considered reached',
                         default=0.5)
     args = parser.parse_args()
     print(args)
 
-    evaluator = SimEvaluator(args.tolerance, args.environment)
+    evaluator = SimEvaluator(args.tolerance, args.environment, args.carla, args.fear_filter)
     evaluator.run(args.model_dirs, args.model_names)
     
 # EoF

viplanner/traj_cost_opt/traj_cost.py
@@ -1,6 +1,7 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
-from typing import Optional
+from typing import Optional, Tuple
 
 torch.set_default_dtype(torch.float32)
 
@@ -78,6 +79,7 @@ class TrajCost:
         waypoints: torch.Tensor,
         odom: torch.Tensor,
         goal: torch.Tensor,
+        fear: torch.Tensor,
         log_step: int,
         ahead_dist: float,
         dataset: str = "train",
@@ -87,10 +89,104 @@ class TrajCost:
         assert self.is_map, "Map has to be set for cost calculation"
         world_ps = self.TransformPoints(odom, waypoints).tensor()
         
+        # Obstacle loss
+        oloss_M = self._compute_oloss(world_ps, batch_size)
+        oloss = torch.mean(torch.sum(oloss_M, axis=1))
+            
+        # Terrian Height loss
+        norm_inds, _ = self.cost_map.Pos2Ind(world_ps)
+        height_grid = self.cost_map.ground_array.T.expand(batch_size, 1, -1, -1)
+        hloss_M = F.grid_sample(height_grid, norm_inds[:, None, :, :], mode='bicubic', padding_mode='border', align_corners=False).squeeze(1).squeeze(1)
+        hloss_M = torch.abs(world_ps[:, :, 2]  - odom[:, None, 2] - hloss_M).to(torch.float32)  # world_ps - odom to have them on the ground to be comparable to the height map
+        hloss_M = torch.sum(hloss_M, axis=1)
+        hloss = torch.mean(hloss_M)
+        
+        # Goal Cost - Control Cost
+        gloss_M = torch.norm(goal[:, :3] - waypoints[:, -1, :], dim=1)
+        # gloss = torch.mean(gloss_M)
+        gloss = torch.mean(torch.log(gloss_M + 1.0))
+        
+        # Moving Loss - punish staying 
+        desired_wp = self.opt.TrajGeneratorFromPFreeRot(goal[:, None, 0:3], step=1.0/(num_p-1)) 
+        desired_ds = torch.norm(desired_wp[:, 1:num_p, :] - desired_wp[:, 0:num_p-1, :], dim=2)
+        wp_ds = torch.norm(waypoints[:, 1:num_p, :] - waypoints[:, 0:num_p-1, :], dim=2)
+        mloss = torch.abs(desired_ds - wp_ds)
+        mloss = torch.sum(mloss, axis=1)
+        mloss = torch.mean(mloss)
+        
+        # Complete Trajectory Loss  
+        trajectory_loss = self.w_obs*oloss + self.w_height*hloss + self.w_motion*mloss + self.w_goal*gloss
+        
+        # Fear labels
+        goal_dists = torch.cumsum(wp_ds, dim=1, dtype=wp_ds.dtype)
+        goal_dists = torch.vstack([goal_dists]*5)
+        floss_M = torch.clone(oloss_M)
+        floss_M[goal_dists > ahead_dist] = 0.0 
+        fear_labels = torch.max(floss_M, 1, keepdim=True)[0]
+        # fear_labels = nn.Sigmoid()(fear_labels-obstalce_thred)
+        fear_labels = fear_labels > self.obstalce_thred + self.neg_reward[2]
+        fear_labels = torch.any(fear_labels.reshape(5, batch_size).T, dim=1, keepdim=True).to(torch.float32)
+        # Fear loss
+        collision_probabilty_loss = nn.BCELoss()(fear, fear_labels.float())
+        
+        # log
+        if self.log_data:
+            try:
+                wandb.log({f"Height Loss {dataset}":        self.w_height*hloss         }, step=log_step)
+                wandb.log({f"Obstacle Loss {dataset}":      self.w_obs*oloss            }, step=log_step)
+                wandb.log({f"Goal Loss {dataset}":          self.w_goal*gloss           }, step=log_step)
+                wandb.log({f"Motion Loss {dataset}":        self.w_motion*mloss         }, step=log_step)
+                wandb.log({f"Trajectory Loss {dataset}":    trajectory_loss             }, step=log_step)
+                wandb.log({f"Collision Loss {dataset}":     collision_probabilty_loss   }, step=log_step)
+            except:
+                print("wandb log failed")
+                
+        # TODO: kinodynamics cost
+        return collision_probabilty_loss+trajectory_loss
+
+    def obs_cost_eval(
+        self,
+        odom: torch.Tensor,
+        waypoints: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Compute Obstacle Loss for eval_sim_static script!
+
+        Args:
+            odom (torch.Tensor): Current odometry
+            waypoints (torch.Tensor): waypoints in camera frame
+
+        Returns:
+            tuple: mean obstacle loss for each trajectory, max obstacle loss for each trajectory
+        """
+        assert self.is_map, "Map has to be loaded for evaluation"
+
+        # compute obstacle loss
+        world_ps = self.TransformPoints(odom, waypoints).tensor()
+        oloss_M = self._compute_oloss(world_ps, waypoints.shape[0])
+        # account for negative reward
+        oloss_M = oloss_M - self.neg_reward[2]
+        oloss_M[oloss_M < 0] = 0.0
+        oloss_M = oloss_M.reshape(-1, waypoints.shape[0], oloss_M.shape[1]) 
+        return torch.mean(oloss_M, axis=[0, 2]), torch.amax(oloss_M, dim=[0, 2])
+    
+    def cost_of_recorded_path(
+        self,
+        waypoints: torch.Tensor,
+    ) -> torch.Tensor:
+        """Cost of recorded path - for evaluation only
+
+        Args:
+            waypoints (torch.Tensor): Path coordinates in world frame
+        """        
+        assert self.is_map, "Map has to be loaded for evaluation"
+        oloss_M = self._compute_oloss(waypoints, 1)
+        return torch.mean(oloss_M)
+    
+    def _compute_oloss(self, world_ps, batch_size):
         # include robot dimension as square
         tangent = world_ps[:, 1:, 0:2] - world_ps[:, :-1, 0:2]  # get tangent vector
         tangent = tangent / torch.norm(tangent, dim=2, keepdim=True)  # normalize normals vector
-        normals = tangent[:, :, [1, 0]] * torch.tensor([-1, 1], dtype=torch.float32, device=waypoints.device)  # get normal vector
+        normals = tangent[:, :, [1, 0]] * torch.tensor([-1, 1], dtype=torch.float32, device=world_ps.device)  # get normal vector
         world_ps_inflated = torch.vstack([world_ps[:, :-1, :]] * 5)  # duplicate points
         world_ps_inflated[:, :, 0:2] = torch.vstack([
             # robot corners
@@ -108,7 +204,6 @@ class TrajCost:
         cost_grid = self.cost_map.cost_array.T.expand(batch_size*5, 1, -1, -1)
         oloss_M = F.grid_sample(cost_grid, norm_inds[:, None, :, :], mode='bicubic', padding_mode='border', align_corners=False).squeeze(1).squeeze(1)
         oloss_M = oloss_M.to(torch.float32)
-        oloss = torch.mean(torch.sum(oloss_M, axis=1))
         
         if self.debug:
             # add negative reward for cost-map
@@ -153,82 +248,7 @@ class TrajCost:
             pcd.colors = o3d.utility.Vector3dVector(sc1.to_rgba(oloss_M[[0, batch_size, 2*batch_size, 3*batch_size, 4*batch_size]].reshape(-1).detach().cpu().numpy())[:, :3])
             # pcd.colors = o3d.utility.Vector3dVector(sc2.to_rgba(cost_values[0].cpu().numpy())[:, :3])
             o3d.visualization.draw_geometries([self.cost_map.pcd_tsdf, pcd])
-            
-        # Terrian Height loss
-        norm_inds, _ = self.cost_map.Pos2Ind(world_ps)
-        height_grid = self.cost_map.ground_array.T.expand(batch_size, 1, -1, -1)
-        hloss_M = F.grid_sample(height_grid, norm_inds[:, None, :, :], mode='bicubic', padding_mode='border', align_corners=False).squeeze(1).squeeze(1)
-        hloss_M = torch.abs(world_ps[:, :, 2]  - odom[:, None, 2] - hloss_M).to(torch.float32)  # world_ps - odom to have them on the ground to be comparable to the height map
-        hloss_M = torch.sum(hloss_M, axis=1)
-        hloss = torch.mean(hloss_M)
         
-        # Goal Cost - Control Cost
-        gloss_M = torch.norm(goal[:, :3] - waypoints[:, -1, :], dim=1)
-        # gloss = torch.mean(gloss_M)
-        gloss = torch.mean(torch.log(gloss_M + 1.0))
+        return oloss_M
         
-        # Moving Loss - punish staying 
-        desired_wp = self.opt.TrajGeneratorFromPFreeRot(goal[:, None, 0:3], step=1.0/(num_p-1)) 
-        desired_ds = torch.norm(desired_wp[:, 1:num_p, :] - desired_wp[:, 0:num_p-1, :], dim=2)
-        wp_ds = torch.norm(waypoints[:, 1:num_p, :] - waypoints[:, 0:num_p-1, :], dim=2)
-        mloss = torch.abs(desired_ds - wp_ds)
-        mloss = torch.sum(mloss, axis=1)
-        mloss = torch.mean(mloss)
-        
-        if self.log_data:
-            try:
-                wandb.log({f"hloss_{dataset}_step": self.w_height*hloss }, step=log_step)
-                wandb.log({f"oloss_{dataset}_step": self.w_obs*oloss    }, step=log_step)
-                wandb.log({f"gloss_{dataset}_step": self.w_goal*gloss   }, step=log_step)
-                wandb.log({f"mloss_{dataset}_step": self.w_motion*mloss }, step=log_step)
-            except:
-                print("wandb log failed")
-                
-        # Fear labels
-        goal_dists = torch.cumsum(wp_ds, dim=1, dtype=wp_ds.dtype)
-        goal_dists = torch.vstack([goal_dists]*5)
-        floss_M = torch.clone(oloss_M)
-        floss_M[goal_dists > ahead_dist] = 0.0 
-        fear_labels = torch.max(floss_M, 1, keepdim=True)[0]
-        # fear_labels = nn.Sigmoid()(fear_labels-obstalce_thred)
-        fear_labels = fear_labels > self.obstalce_thred
-        fear_labels = torch.any(fear_labels.reshape(batch_size, 5), dim=1, keepdim=True).to(torch.float32)
-
-        # TODO: kinodynamics cost
-        return self.w_obs*oloss + self.w_height*hloss + self.w_motion*mloss + self.w_goal*gloss, fear_labels.float()
-
-    def obs_cost_eval(self, odom, waypoints):
-        batch_size, num_p, _ = waypoints.shape
-        world_ps = self.TransformPoints(odom, waypoints)
-        norm_inds, _ = self.cost_map.Pos2Ind(world_ps)
-        
-        # Obstacle Cost
-        cost_grid = self.cost_map.cost_array.T.expand(batch_size, 1, -1, -1)
-        oloss_M = F.grid_sample(cost_grid, norm_inds[:, None, :, :], mode='bicubic', padding_mode='border', align_corners=False).squeeze(1).squeeze(1)
-        oloss_M = oloss_M.to(torch.float32)
-        if self.cost_map.cfg.semantics:
-            neg_reward = self.cost_map.cfg.sem_cost_map.negative_reward
-            oloss_M = oloss_M - neg_reward
-            oloss_M[oloss_M < 0] = 0.0
-        oloss_M_weighted = torch.mean(oloss_M, axis=1)
-        return oloss_M_weighted, torch.max(oloss_M, axis=1)[0]
-    
-    def cost_of_recorded_path(
-        self,
-        waypoints: torch.Tensor,
-    ) -> None:
-        """Cost of recorded path - for evaluation only
-
-        Args:
-            waypoints (torch.Tensor): Path coordinates in world frame
-        """        
-        assert self.is_map, "Map has to be loaded for evaluation"
-        norm_inds, _ = self.cost_map.Pos2Ind(waypoints.unsqueeze(0))
-        
-        # Obstacle Cost
-        cost_grid = self.cost_map.cost_array.T.expand(1, 1, -1, -1)
-        oloss_M = F.grid_sample(cost_grid, norm_inds[:, None, :, :], mode='bicubic', padding_mode='border', align_corners=False).squeeze(1).squeeze(1)
-        oloss_M = oloss_M.to(torch.float32)
-        return torch.mean(oloss_M)
-    
 # EoF

viplanner/utils/trainer.py
@@ -518,16 +518,16 @@ class Trainer:
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         
         waypoints = traj_cost.opt.TrajGeneratorFromPFreeRot(preds, step=step)
-        loss1, fear_labels = traj_cost.CostofTraj(
+        loss = traj_cost.CostofTraj(
             waypoints,
             odom,
             goal,
+            fear,
             log_step,
             ahead_dist=self._cfg.fear_ahead_dist,
             dataset=dataset,
         )
-        loss2 = nn.BCELoss()(fear, fear_labels)
         
-        return loss1+loss2, waypoints 
+        return loss, waypoints 
     
 # EoF